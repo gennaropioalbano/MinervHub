55e983337ff789ea68c8b6c58d52c0400ae21e6b
Jupex <102091983+jupex69@users.noreply.github.com>
2025-12-10 17:26:57 +0100

    fix - Utente.java, UtenteRepository.java

--- src/main/java/it/minervhub/repository/UtenteRepository.java
+++ src/main/java/it/minervhub/repository/UtenteRepository.java
@@ -4,9 +4,6 @@ import it.minervhub.model.Utente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.lang.ScopedValue;
-import java.util.Optional;
-
 @Repository
 public interface UtenteRepository extends JpaRepository<Utente, Long> {
     Utente findByEmail(String email);
